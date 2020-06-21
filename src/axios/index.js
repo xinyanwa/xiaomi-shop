301f26b5de99ecff12c660e24e82c1201c1510ec
xinyanwa <2322246386@qq.com>
2020-06-21 10:44:43 +0800

    完善封装axios

---
+++ src/axios/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
+
+const serve = axios.create({
+  // 根据前端的跨域方式做调整
+  baseURL: '/api',
+  // 设置超时时间
+  timeout: 5000,
+});
+
+// 接口错误拦截
+axios.interceptors.respose.use((response) => {
+  const res = response.data;
+  let data;
+  if (res.status === 0) {
+    data = res.data;
+  } else if (res.status === 10) {
+    window.location.href = '/region';
+  } else {
+    // function alert( res.msg );
+    alert(res.msg);
+    // console.log();
+  }
+  return data;
+});
+
+export default serve;
