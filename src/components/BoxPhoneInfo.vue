9988dba1366867d948f77da88aa6fee1759d4a77
xinyanwa <2322246386@qq.com>
2020-07-10 20:57:55 +0800

    需要明天封装小组件，今天判断样式已经ok，剩下的用来看JavaScript

---
+++ src/components/BoxPhoneInfo.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="box-phone-info">
+        <div class="box-wrapper">
+            <div class="head-text">
+                <h2 class="title">手机</h2>
+            </div>
+            <div class="box-phone">
+                <div class="left-phone">
+                    <ul>
+                        <li
+                                v-for="(item, index) in leftImgData"
+                                :key="index"
+                                :class="item.length === 0 ? '' : 'left-botton-li'"
+                        >
+                            <a href="" :class="item.length === 0 ? '' : 'left-a-img'">
+                                <div class="phone-img">
+                                    <img :src="item" alt="">
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="right-phone"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'BoxPhoneInfo',
+  data() {
+    return {
+      leftImgData: [
+        'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/f256b81e4233333692eece77ed06ebc2.jpg?thumb=1&w=293&h=375&f=webp&q=90',
+        'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e22cf675726deb5eb728f2ccd4cbeafd.jpg?thumb=1&w=293&h=375&f=webp&q=90',
+      ],
+      leftImg: [
+        'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/574c6643ab91c6618bfb2d0e2c761d0b.jpg?thumb=1&w=293&h=768&f=webp&q=90',
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.box-phone-info{
+    background: $colorM;
+    .box-wrapper{
+        @include container();
+        .head-text {
+            .title {
+                @include headTitle();
+            }
+        }
+        .box-phone{
+            @include flex(start);
+            .left-phone{
+                ul{
+                    @include flex( space-between, start);
+                    flex-direction: column;
+                    li{
+                        a{
+                            display: inline-block;
+                            width: 234px;
+                            height: 614px;
+                            .phone-img{
+                                width: 100%;
+                                height: 100%;
+                                img{
+                                    width: 100%;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .right-phone{
+                padding-left: 14px;
+            }
+        }
+    }
+}
+    .left-botton-li {
+        padding-bottom: 14px;
+        &:last-child {
+            padding-bottom: 0;
+        }
+    }
+    .left-a-img {
+            height: 300px !important;
+    }
+</style>
--- src/page/index.vue
+++ src/page/index.vue
@@ -3,6 +3,7 @@
         <banner />
         <flash-sale />
         <home-banner :img-data="imgData"/>
+        <box-phone-info />
     </div>
 </template>
 
@@ -10,24 +11,19 @@
 import Banner from '../components/Banner.vue';
 import FlashSale from '../components/FlashSale.vue';
 import HomeBanner from '../components/HomeBanner.vue';
+import BoxPhoneInfo from '../components/BoxPhoneInfo.vue';
 
 export default {
   name: 'Index',
   data() {
     return {
-      swiperImg: [
-        'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e472d8648df665bc0b2047252685cef0.jpg?w=2452&h=920',
-        'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/01ece47410667e21e9d6ff879d8448b5.jpg?thumb=1&w=1533&h=575&f=webp&q=90',
-        'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/cd53d3541a73e3c5efb500d611a988ad.jpeg?thumb=1&w=1533&h=575&f=webp&q=90',
-        'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/ad9c1cb79a2eeb2d5ccf54dfa9782032.jpg?thumb=1&w=1533&h=575&f=webp&q=90',
-        'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/6ef43cf9f138a7fc3a39273d7e3d13b6.jpg?thumb=1&w=1533&h=575&f=webp&q=90',
-      ],
       imgData: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/17ae6ffbdd4156119e41dec7d85ebced.jpeg?thumb=1&w=1533&h=150&f=webp&q=90',
     };
   },
   computed: {
   },
   components: {
+    BoxPhoneInfo,
     HomeBanner,
     FlashSale,
     Banner,
